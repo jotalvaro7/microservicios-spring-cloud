218ef60cb08133991b41319e4b64bca49e200dba
Julio Osorio <jotalvaro.cc@gmail.com>
2022-10-05 10:54:33 -0500

    implementando servicio de game of thrones con sus dependencias eureka-client, web, actuator, java-faker

---
+++ game-of-thrones/src/main/java/org/personales/gameofthrones/GameOfThronesApplication.java
@@ -0,0 +1,13 @@
+package org.personales.gameofthrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfThronesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOfThronesApplication.class, args);
+	}
+
+}
