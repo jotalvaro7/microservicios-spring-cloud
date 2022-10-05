2ec4d5a66b1faca5b8e3bd4e8ae796e382031db0
Julio Osorio <jotalvaro.cc@gmail.com>
2022-10-05 11:20:49 -0500

    implementando msvc de dragon ball failover y su controlador

---
+++ dragonball-failover/src/main/java/org/personales/dragonballfailover/DragonballFailoverApplication.java
@@ -0,0 +1,13 @@
+package org.personales.dragonballfailover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonballFailoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonballFailoverApplication.class, args);
+	}
+
+}
---
+++ dragonball-failover/src/test/java/org/personales/dragonballfailover/DragonballFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personales.dragonballfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonballFailoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
