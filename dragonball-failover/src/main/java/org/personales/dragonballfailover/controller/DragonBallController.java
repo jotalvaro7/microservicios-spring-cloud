fca4ade406d13e8c1c7027e5b05ed4fb8ebbb01a
Julio Osorio <jotalvaro.cc@gmail.com>
2022-10-05 15:22:06 -0500

    modificando url para el controlador de dragon ball failover

--- dragonball-failover/src/main/java/org/personales/dragonballfailover/controller/DragonBallController.java
+++ dragonball-failover/src/main/java/org/personales/dragonballfailover/controller/DragonBallController.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/dragonball/characters")
+@RequestMapping("/api/v1/db-failover/dragonball/characters")
 public class DragonBallController {
 
     private Faker faker = new Faker();
