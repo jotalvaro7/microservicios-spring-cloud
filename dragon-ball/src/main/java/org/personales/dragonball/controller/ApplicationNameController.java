0ce6157bfd802c15770c6e6696b55acd34f42f7e
Julio Osorio <jotalvaro.cc@gmail.com>
2022-10-06 13:11:54 -0500

    implementando @Timed

--- dragon-ball/src/main/java/org/personales/dragonball/controller/ApplicationNameController.java
+++ dragon-ball/src/main/java/org/personales/dragonball/controller/ApplicationNameController.java
@@ -1,14 +1,17 @@
 package org.personales.dragonball.controller;
 
+import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.personales.dragonball.config.DragonBallConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Random;
 
@@ -30,10 +33,14 @@ public class ApplicationNameController {
 
 
     @GetMapping
+    @Timed("dragonball.name.get")
     public ResponseEntity<String> getAppName(){
         Log.info("Getting application name");
         int value = new Random().nextInt(5);
         registry.counter("dragonball.name", "level", (value<3)?"jr":"sr").increment(value);
+        if(value < 3 ){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
         return ResponseEntity.ok(dragonBallConfig.getApplicationName());
     }
 }
