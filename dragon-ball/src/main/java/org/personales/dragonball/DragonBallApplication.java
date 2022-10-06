454bc13531fe3552f45a739214d569c696ce6cd3
Julio Osorio <jotalvaro.cc@gmail.com>
2022-10-05 22:28:56 -0500

    Agregando algunas anotaciones

--- dragon-ball/src/main/java/org/personales/dragonball/DragonBallApplication.java
+++ dragon-ball/src/main/java/org/personales/dragonball/DragonBallApplication.java
@@ -2,8 +2,10 @@ package org.personales.dragonball;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DragonBallApplication {
 
 	public static void main(String[] args) {
