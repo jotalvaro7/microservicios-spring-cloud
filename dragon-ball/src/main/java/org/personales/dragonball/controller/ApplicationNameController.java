859ce45f103d19f4df90e62ecbd8ccc8007fc70f
Julio Osorio <jotalvaro.cc@gmail.com>
2022-10-06 12:38:58 -0500

    implementando meter registry

--- dragon-ball/src/main/java/org/personales/dragonball/controller/ApplicationNameController.java
+++ dragon-ball/src/main/java/org/personales/dragonball/controller/ApplicationNameController.java
@@ -1,18 +1,25 @@
 package org.personales.dragonball.controller;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.personales.dragonball.config.DragonBallConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 @RestController
 @RequestMapping("/application-name")
 public class ApplicationNameController {
 
     private DragonBallConfig dragonBallConfig;
+
+    @Autowired
+    private MeterRegistry registry;
 
     private static final Logger Log = LoggerFactory.getLogger(ApplicationNameController.class);
 
@@ -25,6 +32,8 @@ public class ApplicationNameController {
     @GetMapping
     public ResponseEntity<String> getAppName(){
         Log.info("Getting application name");
+        int value = new Random().nextInt(5);
+        registry.counter("dragonball.name", "level", (value<3)?"jr":"sr").increment(value);
         return ResponseEntity.ok(dragonBallConfig.getApplicationName());
     }
 }
