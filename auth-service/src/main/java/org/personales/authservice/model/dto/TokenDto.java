7697b7639e29be1de92b061fca9bfd955083edd8
Julio Osorio <jotalvaro.cc@gmail.com>
2022-10-11 11:24:04 -0500

    Creando DTO's

---
+++ auth-service/src/main/java/org/personales/authservice/model/dto/TokenDto.java
@@ -0,0 +1,16 @@
+package org.personales.authservice.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class TokenDto {
+
+    private String token;
+
+}
---
+++ auth-service/src/main/java/org/personales/authservice/model/dto/UserDto.java
@@ -0,0 +1,18 @@
+package org.personales.authservice.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class UserDto {
+    
+    private Long id;
+    private String username;
+    private String password;
+
+}
